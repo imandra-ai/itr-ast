3a328e0d237aa0cfb98f4557128abe217cd2df18
Emma Tye <tyelouiseemma@gmail.com>
2024-06-03 17:03:39 +0100

    chore: fmt

--- test/itr_ast_test.ml
+++ test/itr_ast_test.ml
@@ -1,4 +1,3 @@
-
 let () = Itr_ast_pp.gen_pp "simple_limit.json"
 
 let () = Itr_ast_pp.gen_pp "test1.json"
@@ -8,5 +7,3 @@ let () = Itr_ast_pp.gen_pp "test2.json"
 let () = Itr_ast_pp.gen_pp "test3.json"
 
 let () = Itr_ast_pp.gen_pp "test4.json"
-
-
--- test/pp.ml
+++ test/pp.ml
@@ -1,3 +1,5 @@
 let () =
-  if Array.length Sys.argv < 2 then failwith "no argument specified" else
-  Itr_ast_pp.gen_pp Sys.argv.(1)
+  if Array.length Sys.argv < 2 then
+    failwith "no argument specified"
+  else
+    Itr_ast_pp.gen_pp Sys.argv.(1)
