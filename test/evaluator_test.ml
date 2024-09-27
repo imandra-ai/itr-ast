7c7704f5b81f9d1350346936d7edb81843a8f5b8
Ewen Maclean <ewenmaclean@gmail.com>
2024-09-27 10:52:14 +0100

    chore: fix existing tests

--- test/evaluator_test.ml
+++ test/evaluator_test.ml
@@ -7,7 +7,8 @@ let field_path_pp fmt = function
   | s, Some index -> CCFormat.fprintf fmt "%s[%i]" s (Z.to_int index)
 
 let msg_pp =
-  Itr_evaluator.Field_path_map.pp (CCList.pp field_path_pp) Itr_ast_pp.value_pp
+  Itr_evaluator.Field_path_map.pp (CCList.pp field_path_pp)
+    Itr_ast_pp.record_item_pp
 
 let context msg =
   Itr_evaluator.
@@ -19,8 +20,19 @@ let context msg =
             implicit_message = Some { tag = "Message"; msg };
             get_field =
               (fun ctx path ->
-                Rec_value
-                  (Value (Field_path_map.get_or ~default:lit_none path ctx)));
+                let ctx =
+                  match ctx with
+                  | Msg { msg = ctx; _ } -> ctx
+                  | Record_item ctx ->
+                    (match ctx with
+                    | Rec_record { elements; _ } ->
+                      Field_path_map.of_list
+                        (Itr_ast.String_map.to_list elements
+                        |> CCList.map (fun (s, e) -> [ s, None ], e))
+                    | _ -> Field_path_map.empty)
+                in
+                Field_path_map.get_or ~default:(Rec_value (Value lit_none)) path
+                  ctx);
           };
       field_presence_map =
         Field_path_map.map (fun _ -> Itr_evaluator.Present) msg;
@@ -43,7 +55,7 @@ let () =
   in
   let msg =
     Itr_evaluator.Field_path_map.of_list
-      [ field_path, lit_string "non_default" ]
+      [ field_path, Itr_ast.Rec_value (Value (lit_string "non_default")) ]
   in
   let ctx = context msg in
   let result = Itr_evaluator.evaluate_expr ctx item in
@@ -53,7 +65,10 @@ let () =
     Itr_ast_pp.expr_pp item msg_pp msg Itr_ast_pp.record_item_pp result;
   let msg =
     Itr_evaluator.Field_path_map.of_list
-      [ [ "a_different_field", None ], lit_string "non_default" ]
+      [
+        ( [ "a_different_field", None ],
+          Itr_ast.Rec_value (Value (lit_string "non_default")) );
+      ]
   in
   let ctx = context msg in
   let result = Itr_evaluator.evaluate_expr ctx item in
@@ -85,8 +100,8 @@ let () =
   let msg =
     Itr_evaluator.Field_path_map.of_list
       [
-        field_path_one, lit_string "field_one";
-        field_path_two, lit_string "field_two";
+        field_path_one, Itr_ast.Rec_value (Value (lit_string "field_one"));
+        field_path_two, Itr_ast.Rec_value (Value (lit_string "field_two"));
       ]
   in
   let ctx = context msg in
@@ -102,11 +117,7 @@ let () =
     Itr_ast.(
       Eq
         {
-          lhs =
-            Rec_value
-              (Value
-                 (Literal
-                    (LiteralNone)));
+          lhs = Rec_value (Value (Literal LiteralNone));
           rhs =
             Rec_value
               (Value (MessageValue { var = None; field_path = field_path_one }));
